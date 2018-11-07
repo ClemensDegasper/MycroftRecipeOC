8aa9a613f346eaa2c451da04661754e3461bd835
RaphaelHub <raphael-gruber@hotmail.com>
2018-11-07 14:43:16 +0100

    url in wrapper aufruf neu

--- SPARQLConnector.py
+++ SPARQLConnector.py
@@ -2,17 +2,18 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 def execSPARQLQuery( selectQuery ,whereQuery ):
 	
-	sparql = SPARQLWrapper("http://graphdb.sti2.at:8080/sparql")
+	sparql = SPARQLWrapper("http://graphdb.sti2.at:8080/repositories/broker-graph")
+
+
 
 	query = """
 	PREFIX schema: <http://schema.org/>
 	SELECT  %s
-	#FROM <https://broker.semantify.it/graph/O7PY8ri5T2/WxGcA2Nj1O/latest>
-	FROM <https://broker.semantify.it/graph/O7PY8ri5T2/WxGcA2Nj1O/2018-11-03-09-51>
+	FROM <https://broker.semantify.it/graph/O7PY8ri5T2/WxGcA2Nj1O/latest>
 	WHERE {
 		?s a schema:Recipe .
 		%s
-	} LIMIT 10
+	}
 	""" % (selectQuery, whereQuery)
 
 	sparql.setReturnFormat(JSON)
